384644d25d09cd24b002cc2ab9aecce3351ba463
taladjidi <taladjidi@gmail.com>
2021-01-25 10:58:29 +0100

    Talks to scope

--- test_pyvi.py
+++ test_pyvi.py
@@ -1,7 +1,21 @@
-import visa
-
+import pyvisa as visa
 rm = visa.ResourceManager('@py')
 
 instruments = rm.list_resources()
 
 print(instruments)
+
+for _ in instruments:
+
+    if 'DS' in _:
+
+        oscilloscope = rm.open_resource(_)
+
+    if 'DG' in _:
+
+        generator = rm.open_resource(_)
+    if 'INSTR' in _:
+        instrument = rm.open_resource(_)
+# generator.query("*RST")
+# oscilloscope.query("*RST")
+instrument.query("*RST")
