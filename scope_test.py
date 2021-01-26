d2df7db3c9df62b7d441d76d815c228227586331
Tangui Aladjidi <taladjidi@gmail.com>
2021-01-26 13:36:13 +0100

    Add files via upload

---
+++ scope_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 10:47:25 2021
+
+@author: Tangui ALADJIDI
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+from ScopeInterface import USBScope
+
+rigol = USBScope()
